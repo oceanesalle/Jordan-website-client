3011667d03aed997443317df04de0e6678fa7725
oceansalle <oceanesalleiae@gmail.com>
2022-10-19 11:39:55 +0200

    menu data

---
+++ website/src/data/MenuData.js
@@ -0,0 +1,6 @@
+export const menuData = [
+  { title: 'A propos', link: '/about' },
+  { title: 'Clôtures', link: '/clotures' },
+  { title: 'Portails', link: '/portails' }, 
+  { title: 'Automatismes', link: '/automatismes' }
+];
