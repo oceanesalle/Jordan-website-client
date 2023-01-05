15a08119a55d69c1dd972747e5b0fe7e741cced7
oceansalle <oceanesalleiae@gmail.com>
2023-01-05 10:34:23 +0100

    indexjs reactdom modification

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(
+<BrowserRouter>
+     <App />
+  </BrowserRouter>
+); 
+
